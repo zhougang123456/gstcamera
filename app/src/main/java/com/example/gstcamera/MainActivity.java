b9df22b5b31702fe97950168bfd2cc4d083c8a39
zhougang <1183901440@qq.com>
2020-01-16 13:54:11 +0800

    commit project

---
+++ app/src/main/java/com/example/gstcamera/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.gstcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SurfaceView surfaceView = findViewById(R.id.surface);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                NativeStart(holder.getSurface());
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+    }
+    public static native void NativeStart(Surface surface);
+}
---
+++ app/src/main/jni/camera.h
@@ -0,0 +1,8 @@
+//
+// Created by zhougang on 2020/1/16.
+//
+
+#ifndef GSTCAMERA_CAMERA_H
+#define GSTCAMERA_CAMERA_H
+
+#endif //GSTCAMERA_CAMERA_H
